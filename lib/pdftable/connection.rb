44268a55218626fe616ec37ebc2833120cf6e45d
Edgar <zorro.ej@gmail.com>
2018-06-01 23:46:17 +0800

    parse response

--- lib/pdftable/connection.rb
+++ lib/pdftable/connection.rb
@@ -2,36 +2,41 @@ require 'rest-client'
 require 'pdftable/configuration'
 
 module Pdftable
-  class Connection 
+	class Connection 
 
 		attr_reader :configuration
 
 		def initialize
 			@configuration = Pdftable::Configuration.instance
 		end
-    
-    def get (path)
-      opt = { key: configuration.key }
-      RestClient.get url(path, opt)
-    end
+
+		def get (path)
+			opt = { key: configuration.key }
+			res = RestClient.get url(path, opt)
+			parse_response(res)
+		end
 
 		def post (path, file:, format:)
-      opt = {  key: configuration.key, format: format }
-			RestClient.post url(path, opt), file: File.new(file, 'rb')
+			opt = {  key: configuration.key, format: format }
+			res = RestClient.post url(path, opt), file: File.new(file, 'rb')
+			parse_response(res)
 			#req = RestClient::Request.new method: :post, url:  uri, payload: { multipart: true, file: File.new("pdf_table.pdf", 'rb')}, headers: {params: {key: 'abcde', format: "xml"}}
 			#response = req.execute
-    end
+		end
 
 		private
 
 		def url(path, opt)
 			URI::HTTPS.build(host: configuration.host, path: path, query: URI.encode_www_form(opt)).to_s
 		end
-    
 
-  end
+		def parse_response(res)
+			res.body
+		end
+
+	end
 end
 
 
 
-  
+
